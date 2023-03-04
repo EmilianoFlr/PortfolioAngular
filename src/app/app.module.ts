05d55a4395dd04774bcbead39fb72ceb830c3419
EmilianoFlr <119748231+EmilianoFlr@users.noreply.github.com>
2023-03-04 10:29:13 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { SobremiComponent } from './componentes/sobremi/sobremi.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SobremiComponent,
+    SkillsComponent,
+    EducacionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
